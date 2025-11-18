9b93886d4d2440944cc2dddf8bb1c68def8b2822
paulobiao <54109039+paulobiao@users.noreply.github.com>
2025-11-18 06:24:29 -0500

    Removed Prisma for deployment compatibility

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import * as React from 'react';
-import { ChevronLeft, ChevronRight } from 'lucide-react';
 import { DayPicker } from 'react-day-picker';
 
 import { cn } from '@/lib/utils';
@@ -53,14 +52,11 @@ function Calendar({
         day_hidden: 'invisible',
         ...classNames,
       }}
-      components={{
-        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
-        IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
-      }}
       {...props}
     />
   );
 }
+
 Calendar.displayName = 'Calendar';
 
 export { Calendar };
