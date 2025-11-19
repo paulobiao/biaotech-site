841d02bcb55fe4c44038a1be042367fc38eded6c
paulobiao <54109039+paulobiao@users.noreply.github.com>
2025-11-18 19:34:51 -0500

    Fix next-themes types import for Vercel build

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,9 +1,11 @@
-"use client"
+'use client';
 
-import * as React from "react"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
-import type { ThemeProviderProps } from "next-themes"
+import * as React from 'react';
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
+
+// Tipo seguro baseado no próprio componente do next-themes
+type ThemeProviderProps = React.ComponentProps<typeof NextThemesProvider>;
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
